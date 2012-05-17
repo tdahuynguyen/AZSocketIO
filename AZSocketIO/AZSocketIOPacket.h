fd5bc77059bee3292131ab2b28f9bfa668f02cff
Pat Shields <yeoldefortran@gmail.com>
2012-05-17 11:16:09 -0400

    Updated source file licensing and added appledoc.

--- AZSocketIO/AZSocketIOPacket.h
+++ AZSocketIO/AZSocketIOPacket.h
@@ -3,7 +3,19 @@
 //  AZSocketIO
 //
 //  Created by Patrick Shields on 4/7/12.
-//  Copyright (c) 2012 Patrick Shields. All rights reserved.
+//  Copyright 2012 Patrick Shields
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import <Foundation/Foundation.h>
@@ -20,14 +32,49 @@ typedef enum {
     NOOP
 } MESSAGE_TYPE;
 
+/**
+ `AZSocketIOPacket` is an object that represents an encoded message that can be read by the socket.io server.
+ */
 @interface AZSocketIOPacket : NSObject
-@property(nonatomic, assign)int type;
-@property(nonatomic, strong)NSString *Id;
-@property(nonatomic, assign)BOOL ack;
-@property(nonatomic, strong)NSString *endpoint;
-@property(nonatomic, strong)id data;
+/**
+ The type of the message.
+ */
+@property(nonatomic, assign)MESSAGE_TYPE type;
 
+/**
+ The id of the message. This is used for ack'ing packets.
+ */
+@property(nonatomic, strong)NSString *Id;
+
+/**
+ Determines whether the server will ACK a packet. If 'YES', ACK packet will be sent.
+ */
+@property(nonatomic, assign)BOOL ack;
+
+/**
+ The endpoint for the packet. Currently unused.
+ */
+@property(nonatomic, strong)NSString *endpoint;
+
+/**
+ The data to be appended to the packet. This should be preformated to match the type.
+ */
+@property(nonatomic, strong)NSString *data;
+
+/**
+ Initializes a packet using a serialized representation.
+ 
+ @param packetString A serialized packet.
+ 
+ @return the initialized packet.
+ */
 - (id)initWithString:(NSString *)packetString;
+
+/**
+ Serializes a packet so that it can be written to the wire.
+ 
+ @return A string containing the serialized packet data.
+ */
 - (NSString *)encode;
 @end
 
