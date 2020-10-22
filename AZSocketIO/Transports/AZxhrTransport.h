483f18aee924d8be39b2b2b712ab09d335fc73b0
Horseman <huy.nguyen@tda.company>
2020-10-22 15:20:17 +0700

    change header

--- AZSocketIO/Transports/AZxhrTransport.h
+++ AZSocketIO/Transports/AZxhrTransport.h
@@ -20,7 +20,7 @@
 
 #import <Foundation/Foundation.h>
 #import "AZSocketIOTransport.h"
-#import <AFNetworking.h>
+#import "AFHTTPSessionManager.h"
 
 @interface AZxhrTransport : NSObject <AZSocketIOTransport>
 @property(nonatomic, strong)AFHTTPSessionManager *client;
